03b259616a869c9051506b28d41eedc723c4fcff
notsu <literallysum@gmail.com>
2025-02-05 23:09:51 +0500

    initial commit

---
+++ IconConvert.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+
+namespace covericonator
+{
+    internal class IconConvert
+    {
+        // thx https://stackoverflow.com/questions/21387391
+        internal static Icon IconFromImage(Image img)
+        {
+            var ms = new MemoryStream();
+            var bw = new BinaryWriter(ms);
+
+            // Header
+            bw.Write((short)0);   // 0 : reserved
+            bw.Write((short)1);   // 2 : 1=ico, 2=cur
+            bw.Write((short)1);   // 4 : number of images
+
+            // Image directory
+            var w = img.Width;
+            if (w >= 256) w = 0;
+            bw.Write((byte)w);    // 0 : width of image
+            var h = img.Height;
+            if (h >= 256) h = 0;
+            bw.Write((byte)h);    // 1 : height of image
+            bw.Write((byte)0);    // 2 : number of colors in palette
+            bw.Write((byte)0);    // 3 : reserved
+            bw.Write((short)0);   // 4 : number of color planes
+            bw.Write((short)0);   // 6 : bits per pixel
+            var sizeHere = ms.Position;
+            bw.Write(0);     // 8 : image size
+            var start = (int)ms.Position + 4;
+            bw.Write(start);      // 12: offset of image data
+
+            // Image data
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            var imageSize = (int)ms.Position - start;
+            ms.Seek(sizeHere, SeekOrigin.Begin);
+            bw.Write(imageSize);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            return new Icon(ms);
+        }
+
+        internal static void WriteFile(Icon icon, string path)
+        {
+            using (FileStream s = File.Create(path))
+            {
+                icon.Save(s);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace covericonator
+{
+    internal class Program
+    {
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern bool WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        static string? RecursiveSearch(string path, string pattern)
+        {
+            if (!Directory.Exists(path))
+            {
+                if (File.Exists(path))
+                    throw new ArgumentException("Directory needed but a file was passed.");
+                throw new DirectoryNotFoundException();
+            }
+
+            string? found = null;
+            foreach (string file in Directory.EnumerateFiles(path))
+            {
+                if (!Regex.IsMatch(file, pattern)) continue;
+                found = file;
+                break;
+            }
+            if (found != null) return found;
+
+            foreach (string dir in Directory.EnumerateDirectories(path))
+            {
+                string? search = RecursiveSearch(dir, pattern);
+                if (search == null) continue;
+                found = search;
+                break;
+            }
+
+            return found;
+        }
+
+        static void Process(string path)
+        {
+            try
+            {
+                Console.WriteLine($"Processing: {path}");
+                string? cover = RecursiveSearch(path, @"(C|c)over\.(jpe?g|png)");
+                if (cover == null)
+                    throw new FileNotFoundException("Could not find a cover image.");
+                Console.WriteLine($"Cover found at: {cover}");
+
+                Image src = Image.FromFile(cover);
+                Console.WriteLine("Converting to icon...");
+                Bitmap img = new Bitmap(src, 256, 256);
+                Icon icon = IconConvert.IconFromImage(img);
+                string icoPath = Environment.ExpandEnvironmentVariables($"%LocalAppData%\\covericonator\\");
+                if (!Directory.Exists(icoPath)) Directory.CreateDirectory(icoPath);
+                icoPath += $"{Guid.NewGuid()}.ico"; // yes, there's no checking for an existing icon, but who cares
+                IconConvert.WriteFile(icon, icoPath);
+
+                Console.WriteLine("Updating directory...");
+                string confPath = path + "\\desktop.ini";
+                bool ret = WritePrivateProfileString(".ShellClassInfo", "IconResource", icoPath + ",0", confPath);
+                if (!ret)
+                    throw new Exception($"desktop.ini write failed.");
+
+                // TODO: something regarding icon caching?
+                // right now, upon assigning the IconResource windows will display a
+                // broken version of the icon, and you need to open folder properties,
+                // go to the icon tab, and click OK. Yes, you did nothing, but you did
+                // something, and now your icon works properly. What a great OS!
+                Console.WriteLine("Success!");
+            } catch (Exception e)
+            {
+                Console.Error.WriteLine($"{e.GetType().Name}: {e.Message}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                foreach (string arg in args) Process(arg);
+            }
+            else Console.Error.WriteLine("You must pass at least one directory argument.");
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
